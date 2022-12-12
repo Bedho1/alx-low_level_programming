529ac81f14d94dd2f79a07215047adf391a52e08
bedho1 <bedada.r@gmail.com>
2022-12-12 12:17:57 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c != 'z')
+				putchar(c);
+			c++;
+		}
+	puchar('\n');
+	return (0);
+}
