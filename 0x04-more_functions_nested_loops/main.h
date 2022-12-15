998e969977534416f05028ebdefe9aebcccdfd91
bedho1 <bedada.r@gmail.com>
2022-12-15 15:44:48 -0800

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * file - main.h
+ * desc: Header contains all prototype of functions
+ *    used in 0x04-more_functions_nested_loops
+ */
+int _putchar(char c);
+int _isupper(int c);
+
+
+
+
+
+#endif
