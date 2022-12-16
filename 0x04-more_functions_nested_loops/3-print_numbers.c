b8f7406e827a116bf20ae4a8f62f8216d420d0e4
bedho1 <bedada.r@gmail.com>
2022-12-15 16:44:28 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print num from 0-9
+ * return: void
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_puchar(c);
+	}
+	_putchar('\n');
+}
