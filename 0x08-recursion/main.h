1398f6b194aa58184ea6860ec46ff30361facc67
bedho1 <bedada.r@gmail.com>
2023-01-07 03:56:46 -0800

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,6 @@ int is_divisible(int num, int div);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
+int _strlen_recursion(char *s);
+int check_palindrome(char *s, int i, int len);
 #endif
